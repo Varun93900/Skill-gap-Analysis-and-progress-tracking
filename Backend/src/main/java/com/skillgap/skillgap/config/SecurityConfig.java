7299ef022fa3fd7589552a55511a1ff286c3c1a1
Varun93900 <varunallam870@gmail.com>
2026-09-04 21:23:36 +0530

    Enable CORS for deployed frontend

--- Backend/src/main/java/com/skillgap/skillgap/config/SecurityConfig.java
+++ Backend/src/main/java/com/skillgap/skillgap/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.config.Customizer;
 
 
 @Configuration
@@ -30,7 +31,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .csrf(csrf -> csrf.disable())
+    .cors(Customizer.withDefaults())
+    .csrf(csrf -> csrf.disable())
 
                 .sessionManagement(session ->
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
